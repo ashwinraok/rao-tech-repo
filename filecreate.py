6e83ac6a964f5e80289aa1638de625c245ed5766
ashwinraok <ashwinbharath974@gmail.com>
2026-09-25 20:06:14 +0530

    saved

---
+++ filecreate.py
@@ -0,0 +1,6 @@
+with open("test1.pdf", "w") as file:
+    file.write("Hello, World!\n")
+    file.write("This file was created using Python file handling.\n")
+    file.write("Added third line, version 2.")
+
+print("File created successfully!")
